302d94930bc4c1cb19c2956ce4f39d200cc5fe33
JorgeCleres <jrgcleres@hotmail.com>
2022-03-24 09:55:57 -0300

    parece que aqui ja foi

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const shortUrl = require("node-url-shortener");
 const router = express.Router()
 
 router.get('/api', (req, res) => {
-    shortUrl.short("https://www.empiric.com.br", function (err, url) {
-        res.status(200).send({
-            success: true,
-            message: url
-        })
+    shortUrl.short(req.body,  (err, url) => {
+        try {
+            res.status(200).json({message: 'success', url })
+        }
+        catch {
+            res.status(400).json({message: 'erro', err})
+        }
     });
 })
 
