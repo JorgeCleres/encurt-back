a5057b15434958785c757e7b0651d22dd8393017
JorgeCleres <jrgcleres@hotmail.com>
2022-03-24 22:09:10 -0300

    finalizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const shortUrl = require("node-url-shortener");
 const router = express.Router()
 
-router.get('/api', (req, res) => {
-    shortUrl.short(req.body,  (err, url) => {
+router.post('/api', (req, res) => {
+    console.log(req.query.url);
+    shortUrl.short(req.query.url, (err, url) => {
         try {
-            res.status(200).json({message: 'success', url })
+            res.status(200).json({message: 'url', url})
         }
         catch {
             res.status(400).json({message: 'erro', err})
