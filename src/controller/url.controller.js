4de3e5eb08f31bf180a59998aed5bc36215b7c31
JorgeCleres <jrgcleres@hotmail.com>
2022-03-26 14:54:25 -0300

    alterado req

--- src/controller/url.controller.js
+++ src/controller/url.controller.js
@@ -2,7 +2,7 @@ const shortUrl = require("node-url-shortener");
 
 exports.returnUrl = async(req, res) => { 
     try {
-        shortUrl.short(req.query.url, (err, url) => {
+        shortUrl.short(req.body.url, (err, url) => {
             res.status(200).json({message: 'url', url})
         })
     } catch {
